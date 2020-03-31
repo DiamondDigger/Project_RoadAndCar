84a2cb88d567fcac2a623da9450ee4d714f857ee
naglec13 <naglec13@gmail.com>
2020-04-01 01:08:06 +0300

    ..made a predefined cursor like a ring

--- src/ExperimentalWithGraphics/CreateWindow.java
+++ src/ExperimentalWithGraphics/CreateWindow.java
@@ -5,10 +5,13 @@ import java.awt.*;
 
 public class CreateWindow extends JFrame {
 
-    CreateWindow(String s) {
+    CreateWindow(String s) throws AWTException {
         super(s);
         setLayout(null);
+        setTitle("CurrentWindow");
+        setCursor(Cursor.getPredefinedCursor(3));
         setSize(800, 800);
+        setExtendedState(3);
         setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -16,7 +19,7 @@ public class CreateWindow extends JFrame {
     public void paint(Graphics window) {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
         CreateWindow createWindow = new CreateWindow("Window - created");
 
     }
